8e459585da2537c763738acdb426736893c95e5d
GeekchanskiY <astrovskiy.04@gmail.com>
2023-11-20 16:39:13 +0100

    upd

--- objects.py
+++ objects.py
@@ -6,14 +6,23 @@ class Document(object):
     regex_link: str = r'\[\[[^\]]*\]\]'
     class SubDocument(object):
         text: list[str] = []
-        def __init__(self, title: str, parent_doc: object|None) -> None:
+        subdocumets: list = []
+        layer: int = 0
+        def __init__(self, title: str, parent_doc: 'SubDocument or None') -> None:
             if parent_doc is not None and type(parent_doc) == type(self):
                 self.parent_doc = parent_doc
+                self.layer = self.parent_doc.layer + 1
             else:
                 parent_doc = None
         
         def add_line(self, text: str) -> None:
             self.text.append(text)
+        
+        def add_subdocument(self, subdocument: object):
+            if type(subdocument) != type(self):
+                raise ValueError('Subdocument can only be an instance of this class!')
+            if subdocument.layer >= self.layer:
+                raise RuntimeError('Can\'t add child subdocument with the same layer as parent one')
 
     
 
@@ -31,12 +40,32 @@ class Document(object):
 
         with open(file=filepath, mode='r', encoding='utf-8') as f:
             self.text = [l.replace('\n', '') for l in f.readlines()]
-            
+
         self.title = filepath.split('/')[-1].replace('.md', '')
         self.root_doc = self.SubDocument('root', None)
+        current_document_level: int = 0
+        current_subdocument_queue: list[self.SubDocument] = []
+        
         for r in self.text:
-            if r.find('#') == -1:
-                self.root_doc.add_line(r)
+            line_layer_finder: int = 0
+
+            for x in r:
+                if x == '#': line_layer_finder += 1
+                else: break
+            
+            if line_layer_finder != 0:
+
+                if line_layer_finder >= current_document_level:
+                    if current_subdocument_queue:
+                        pass
+                    else:
+                        new_subdocument = self.SubDocument(title=r.replace('#', '').strip(), parent_doc=self.root_doc)
+                        current_subdocument_queue.append(new_subdocument)
+                else:
+                    pass
+            else:
+                if current_subdocument_queue:
+                    pass
             # print(re.findall(self.regex_link, r))
         # print(self.title)
 
